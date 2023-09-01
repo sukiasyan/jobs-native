a4d498c881b02bef01917de847481dd35767627a
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-09-02 00:06:24 +0200

    init setup

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}></TextInput>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
