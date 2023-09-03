cbb8453cd356f40e432c6c97121b3208afe9f19c
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-09-03 17:20:21 +0200

    job details

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -7,28 +7,25 @@ import { checkImageURL } from '../../../../utils';
 const NearbyJobCard = ({ job, handleNavigate }) => {
 console.log('Function: NearbyJobCard - Line 8 - ', JSON.stringify(job));
     return (
-        <TouchableOpacity
-            style={styles.container}
-            onPress={handleNavigate}
-        >
+        <TouchableOpacity style={styles.container} onPress={handleNavigate}>
             <TouchableOpacity style={styles.logoContainer}>
                 <Image
                     source={{
-                        uri: checkImageURL(job?.employer_logo)
+                        uri: checkImageURL(job.employer_logo)
                             ? job.employer_logo
-                            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg',
+                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
                     }}
                     resizeMode='contain'
-                    style={styles.logoImage}
+                    style={styles.logImage}
                 />
             </TouchableOpacity>
 
-
             <View style={styles.textContainer}>
                 <Text style={styles.jobName} numberOfLines={1}>
-                    {job.job_title}
+                    {job?.job_title}
                 </Text>
-                    <Text style={styles.jobType}> {job.job_employment_type}</Text>
+
+                <Text style={styles.jobType}>{job?.job_employment_type}</Text>
             </View>
         </TouchableOpacity>
     );
