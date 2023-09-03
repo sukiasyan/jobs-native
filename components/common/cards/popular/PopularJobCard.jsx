ad65b880b977617f5194774afb008ef3108b4fc8
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-09-03 07:49:46 +0200

    nearby job card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -5,7 +5,7 @@ import styles from './popularjobcard.style';
 import { checkImageURL } from '../../../../utils';
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
-  console.log('XXXXXX - ', item);
+
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
